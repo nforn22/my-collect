5075e5bb545ee62169b96a7812eda597371947be
nforn22 <fornarinicolas22@gmail.com>
2025-05-23 12:39:04 +0200

    Tested and verified API endpoints

--- my-collect-Backend/src/index.js
+++ my-collect-Backend/src/index.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/auth');
 const itemRoutes = require('./routes/item');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
@@ -27,6 +27,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connecté à MongoDB'))
   .catch(err => console.error('Erreur de connexion à MongoDB:', err));
 
+  const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
